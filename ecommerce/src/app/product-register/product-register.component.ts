2e529d51fb8adf5c0810ffa0bec897f2767865d3
Salvador <thiago.salvadorpower@gmail.com>
2019-10-22 15:13:23 -0300

    Fazendo upload de imagens

--- ecommerce/src/app/product-register/product-register.component.ts
+++ ecommerce/src/app/product-register/product-register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-product-register',
   templateUrl: './product-register.component.html',
@@ -7,16 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductRegisterComponent implements OnInit {
 
+  fileData: File = null;
+  previewUrl:any = null;
+  fileUploadProgress: string = null;
+  uploadedFilePath: string = null;
+
   constructor( ) { }
 
   ngOnInit() {
   }
 
 
-  onSubmit() {
-  
+  fileProgress(fileInput: any) {
+    this.fileData = <File>fileInput.target.files[0];
+    this.preview();
+}
+
+preview() {
+  // Show preview 
+  var mimeType = this.fileData.type;
+  if (mimeType.match(/image\/*/) == null) {
+    return;
   }
 
-  
+  var reader = new FileReader();      
+  reader.readAsDataURL(this.fileData); 
+  reader.onload = (_event) => { 
+    this.previewUrl = reader.result; 
+  }
+}
+ 
+onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.fileData);
+    // this.http.post('url/to/your/api', formData)
+    //   .subscribe(res => {
+    //     console.log(res);
+        // this.uploadedFilePath = res.data.filePath;
+        // alert('SUCCESS !!');
+      
+}
+
 
 }
